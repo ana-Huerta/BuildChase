2da46b88d9326c2d7de5a872dfec3919117a72bd
IWantToBeTsugurific <lalahago14@gmail.com>
2026-03-10 14:59:08 -0600

    10 de marzo de 2026
    Creacion de modelos:
    -Character
    -Cards
    -Skills

---
+++ services/umamusume-service/models/cards.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+
+  uniquePerk: { type: String, required: true },
+
+  character: { type: mongoose.Schema.Types.ObjectId, ref: 'Character', required: true },
+
+  rarity: { type: String, enum: ['R', 'SR', 'SSR'], required: true },
+
+  imageFull: { type: String, required: true },
+
+  iconImage: { type: String, required: true },
+
+  type: { type: String, enum: ['Speed', 'Stamina', 'Power', 'Guts', 'Wit'], required: true },
+
+  styles: [{
+    frontRunner: String,
+    paceChaser: String,
+    lateSurger: String,
+    endCloser: String
+  }],
+
+  skills: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Skills'
+  }],
+
+  effects: [{
+    type: String,
+    description: String,
+    value: Number,
+    unlockLevel: Number
+  }],
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Card', cardSchema);
---
+++ services/umamusume-service/models/skills.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const skillSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+
+  iconImage: { type: String, required: true },
+
+  type: { type: String, enum: ['Unique Skill', 'Passive', 'Early Race', 'Mid Race', 'Late Race', 'Any Time'], required: true },
+
+  cost: { type: Number, required: true },
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Skill', skillSchema);
